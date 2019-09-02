dbb4a99b4f5299a77d79b060499a04bfd792c213
Sanking-lee <1310179113@qq.com>
2019-09-02 20:12:25 +0800

    GraphTopSort

--- Main.cpp
+++ Main.cpp
@@ -2,9 +2,12 @@
 #include "Graph.h"
 
 extern void TestGraphTraverse();
+extern void TestGraphTopSort();
 
 int main(){
-    cout << "Test GraphTraverse: \n";
-    TestGraphTraverse();
+    // cout << "Test GraphTraverse: \n";
+    // TestGraphTraverse();
+    cout << "Test GraphTopSort: \n";
+    TestGraphTopSort();
     return 0;
 }
